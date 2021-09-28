10564c8284b22055692300db38fdd9c38f017f79
Matthew Wiessing <matthewwiessing@gmail.com>
2021-09-28 16:34:03 -0500

    added url for badges and made a sample readme

---
+++ READ.md
@@ -0,0 +1,28 @@
+# README generator
+## Description
+It makes a readme from user inputs
+## Table of Contents 
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Questions](#questions)
+##Installation
+make sure node,fs and inquirer are installed
+## Usage
+use node to execute index.js
+## Credits
+undefined.
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+##Contributing
+Ask for permission first
+##Tests
+try to execute it
+##Questions
+You can contact me at:
+--[Github](https://github.com/kiasiri)
+--Email: matthewwiessing@gmail.com
+
+## Badges
+
+
--- index.js
+++ index.js
@@ -4,18 +4,13 @@ const promptUser = () => {
   return inquirer.prompt([
     {
       type: "input",
-      name: "name",
-      message: "What is your name?",
+      name: "title",
+      message: "What is the title of your project?",
     },
     {
       type: "input",
       name: "description",
-      message: "Please type a description of your project",
-    },
-    {
-      type: "input",
-      name: "title",
-      message: "What is the title of your project?",
+      message: "Please describe your project.",
     },
     {
       type: "input",
@@ -37,6 +32,22 @@ const promptUser = () => {
       name: "email",
       message: "What is your email?",
     },
+    {
+      type: "list",
+      name: "license",
+      message: "Which licence applies to your project?",
+      choices: ["MIT", "ISC"],
+    },
+    {
+      type: "input",
+      name: "contribute",
+      mesage: "How can another user contribute to this project?",
+    },
+    {
+      type: "input",
+      name: "tests",
+      message: "How does one test the application?",
+    },
   ]);
 };
 
@@ -49,6 +60,8 @@ const generateReadME = ({
   credit,
   usage,
   license,
+  contribute,
+  tests,
 }) =>
   `# ${title}
 ## Description
@@ -65,13 +78,13 @@ ${usage}
 ## Credits
 ${credit}.
 ## License
-${license}
+[![License: ${license}](https://img.shields.io/badge/License-${license}-yellow.svg)](https://opensource.org/licenses/${license})
 ##Contributing
 ${contribute}
 ##Tests
 ${tests}
 ##Questions
-You can contact me at.
+You can contact me at:
 --[Github](https://github.com/${github})
 --Email: ${email}
 
@@ -82,7 +95,7 @@ You can contact me at.
 const begin = () => {
   promptUser()
     // Use writeFileSync method to use promises instead of a callback function
-    .then((answers) => fs.writeFileSync("README.md", generateReadME(answers)))
+    .then((answers) => fs.writeFileSync("READ.md", generateReadME(answers)))
     .then(() => console.log("Successfully wrote to README"))
     .catch((err) => console.error(err));
 };
